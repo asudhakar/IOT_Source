654c65309eda3f558c1911e1676ce9c493114039
chandanaram <chandanaram51@gmail.com>
2016-05-03 22:18:44 +0530

    testing

--- write_data.php
+++ write_data.php
@@ -4,7 +4,9 @@ $conn = db_connect();
 $moisture = $_GET['moisture'];
 $mic = $_GET['mic'];
 $sql="INSERT INTO icradle (moisture, mic) VALUES ('".$moisture."','".$mic."')";
-mysqli_query($conn,$query);
+if(mysqli_query($conn,$query)){
+	echo "Inserted";
+}
 
 
 
