345da227660a9e664e55a42225440e5e305a4e09
chandanaram <chandanaram51@gmail.com>
2016-05-03 20:58:10 +0530

    Final backend code

---
+++ write_data.php
@@ -0,0 +1,18 @@
+<?php
+$dbusername="root";
+$dbpassword="icradle";
+$server="localhost";
+$dbname = "project";
+$moisture = $_GET['moisture'];
+$mic = $_GET['mic'];
+
+$conn = mysqli_connect($server, $dbusername, $dbpassword, $dbname);
+
+$sql="INSERT INTO icradle (moisture, mic) VALUES ('".$moisture."','".$mic."')";
+mysqli_query($conn, $sql);
+
+echo "success";
+?>
+
+
+
