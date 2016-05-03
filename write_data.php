12a22c086d60f93b9c4c640f6d145d46fad9a0a8
chandanaram <chandanaram51@gmail.com>
2016-05-03 22:16:45 +0530

    bug cleared

--- write_data.php
+++ write_data.php
@@ -2,7 +2,7 @@
 include_once 'functions.php';
 $conn = db_connect();
 $moisture = $_GET['moisture'];
-$mic = $_GET['mic']
+$mic = $_GET['mic'];
 $sql="INSERT INTO icradle (moisture, mic) VALUES ('".$moisture."','".$mic."')";
 mysqli_query($conn,$query);
 
