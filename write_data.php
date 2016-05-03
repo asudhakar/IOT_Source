443c7ee5c40da70b46ad456f9fbc994c59a650f9
chandanaram <chandanaram51@gmail.com>
2016-05-03 22:12:08 +0530

    Added files via upload

--- write_data.php
+++ write_data.php
@@ -2,7 +2,7 @@
 include_once 'functions.php';
 $conn = db_connect();
 $sql="INSERT INTO icradle (moisture, mic) VALUES ('".$moisture."','".$mic."')";
-execute_query($conn, $sql);
+mysqli_query($conn,$query);
 
 
 
