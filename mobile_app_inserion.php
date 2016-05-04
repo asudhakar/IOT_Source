6544537ebf4e5316d8e2ae0599a7243d405acf0d
chandanaram <chandanaram51@gmail.com>
2016-05-04 15:39:21 +0530

    mobile app retuns json

--- mobile_app_inserion.php
+++ mobile_app_inserion.php
@@ -8,25 +8,42 @@ $input = $_GET['input'];
 switch ($input) {
 	case 'servo':
 		if(insert(1, $conn)){
-			echo "1 inserted";
+			inserted();
+		} else{
+			not_inserted();
 		}
 		break;
 	case 'mp3':
-		insert(2, $conn);
-		echo "2 inserted";
+		if(insert(2, $conn)){
+			inserted();
+		} else{
+			not_inserted();
+		}
 		break;
 	case 'blower':
-		insert(3, $conn);
-		echo "3 inserted";
+		if(insert(3, $conn)){
+			inserted();
+		} else{
+			not_inserted();
+		}
 		break;
 	default:
 		echo "Sorry";
 		break;
 }
 function insert($value, $conn){
-	echo $value;
 	$sql = "INSERT INTO app1 (fromapp) VALUES('".$value."')";
-	echo "$sql";
 	mysqli_query($conn, $sql);
 	return true;
 }
+
+function inserted(){
+	$arr = array ('inserted');
+	echo json_encode($arr);
+}
+
+function not_inserted(){
+	$arr = array ('not inserted');
+	echo json_encode($arr);
+}
+
